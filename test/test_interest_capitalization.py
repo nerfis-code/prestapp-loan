e82c1dc8a5cda6f2e7a3cf90daf35e68691c9e35
nerfis-code <nerfisalcantara2017@gmail.com>
2026-02-24 13:46:46 -0400

    Agregar test de plazos y capitalizacion de interés

---
+++ test/test_interest_capitalization.py
@@ -0,0 +1,14 @@
+# import unittest
+# from date_utils import DateUtils
+# from loan import Loan
+
+# class TestInterestCapitalization(unittest.TestCase):
+#     def test_capitalización_del_interés(self):
+#         d = DateUtils("2026-01-01")
+#         loan = Loan(100_000, 0.2, 30, 10, [], "2026-01-01", d.future(65))
+        
+#         self.assertEqual(loan.installments[0].status, "mora")
+        
+# if __name__ == '__main__':
+#     unittest.main()
+      

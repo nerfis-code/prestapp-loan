90554d1d8c0294c1c04ffa2b26a2ea67683089b6
nerfis-code <nerfisalcantara2017@gmail.com>
2026-02-24 15:50:59 -0400

    Eliminar main

--- main.py
+++
@@ -1,9 +0,0 @@
-import json
-import pandas
-from datetime import datetime, timedelta
-from zoneinfo import ZoneInfo
-from loan import Loan
-
-if __name__ == "__main__":
-  today = datetime.now(ZoneInfo("America/Santo_Domingo"))
-

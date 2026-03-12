d22d6f935609775d6e827e251b9995077618d323
nerfis-code <nerfisalcantara2017@gmail.com>
2026-03-11 22:34:56 -0400

    Agregando pruebas de capitalizacion de interes

--- test/test_interest_capitalization.py
+++ test/test_interest_capitalization.py
@@ -1,14 +1,16 @@
-# import unittest
-# from date_utils import DateUtils
-# from loan import Loan
+import unittest
+from date_utils import DateUtils
+from loan import Loan
 
-# class TestInterestCapitalization(unittest.TestCase):
-#     def test_capitalización_del_interés(self):
-#         d = DateUtils("2026-01-01")
-#         loan = Loan(100_000, 0.2, 30, 10, [], "2026-01-01", d.future(65))
+class TestInterestCapitalization(unittest.TestCase):
+    def test_capitalización_del_interés(self):
+        d = DateUtils("2026-01-01")
+        loan = Loan(100_000, 0.2, 30, 10, [], "2026-01-01", d.future(65))
         
-#         self.assertEqual(loan.installments[0].status, "mora")
+        self.assertEqual(loan.installments[0].status, "mora")
+        self.assertEqual(loan.installments[0].interest, 20_000)
+        self.assertNotEqual(loan.installments[-1].interest, 20_000)
         
-# if __name__ == '__main__':
-#     unittest.main()
+if __name__ == '__main__':
+    unittest.main()
       
